95156a884b879118f05eb31b17111d3d9032b872
EmmDevX <elevatedtechnology2023@gmail.com>
2026-07-17 23:00:11 +0100

    Init Commit

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -40,3 +40,97 @@ document.querySelectorAll("#navLinks a").forEach(link => {
         hamburger.classList.remove("active");
     });
 });
+
+// Fixed Navbar 
+const navbar = document.getElementById("navbar");
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > header.offsetHeight) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+
+});
+//Navigation
+const sections = document.querySelectorAll(".section");
+const dots = document.querySelectorAll(".dot");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 200;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    dots.forEach(dot => {
+
+        dot.classList.remove("active");
+
+        if(dot.getAttribute("href") === "#" + current){
+            dot.classList.add("active");
+        }
+
+    });
+
+});
+
+//Slideshow
+const slides = document.querySelectorAll(".slide");
+
+let current = 0;
+
+function nextSlide() {
+
+    // Remove active class
+    slides[current].classList.remove("active");
+
+    // Stop video if current slide is a video
+    if (slides[current].tagName === "VIDEO") {
+        slides[current].pause();
+    }
+
+    // Move to next slide
+    current = (current + 1) % slides.length;
+
+    // Show next slide
+    slides[current].classList.add("active");
+
+    // If next slide is a video
+    if (slides[current].tagName === "VIDEO") {
+
+        slides[current].currentTime = 0;
+        slides[current].play();
+
+        // Wait until video ends before moving on
+        slides[current].onended = nextSlide;
+
+    } else {
+
+        // Images stay for 5 seconds
+        setTimeout(nextSlide, 5500);
+
+    }
+
+}
+
+// Start the slideshow
+showFirst();
+
+function showFirst(){
+
+    slides[0].classList.add("active");
+
+    setTimeout(nextSlide,5500);
+
+}
